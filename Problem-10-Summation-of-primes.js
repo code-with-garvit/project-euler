7bd8a44d80fe5ab085c926d309fd61a2be9a9a6e
code-with-garvit <63104084+code-with-garvit@users.noreply.github.com>
2020-05-30 01:40:52 +0530

    Add files via upload

---
+++ Problem-10-Summation-of-primes.js
@@ -0,0 +1,27 @@
+function primeSummation(n) {
+    let nums = []
+    let sum = 0;
+    const upperBound = Math.ceil(Math.sqrt(n));
+    for (let i = 0; i < n; i++){
+      nums.push(i);   
+    }
+    nums[1] = 0;
+    
+    for (let i = 2; i <= upperBound; i++){
+      if (nums[i] != 0){ 
+        for (let j = i*i; j < n; j += i){
+          if (nums[j] % i === 0) {
+            nums[j] = 0;
+          }
+        }
+      }
+    }
+  
+    for (let i = 0; i < n; i++){
+      sum += nums[i];
+    }
+    return sum;
+  }
+  
+console.log(primeSummation(2000000));
+  
