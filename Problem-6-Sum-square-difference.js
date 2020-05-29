d90a2cdf0d2b11ee27d8d62941b6280cb548df83
code-with-garvit <63104084+code-with-garvit@users.noreply.github.com>
2020-05-29 23:57:00 +0530

    Add files via upload

---
+++ Problem-6-Sum-square-difference.js
@@ -0,0 +1,8 @@
+function sumSquareDifference(n) {
+    const sumOfN = (n * (n + 1)) / 2;
+    const sumOfNSquare = (n * (n + 1) * (2 * n + 1)) / 6;
+    return (sumOfN ** 2) - sumOfNSquare;
+}
+  
+console.log(sumSquareDifference(100));
+  
